ba72a0b0027d2cde840bd11030105097cf04bcc8
airwickzoo <ezhu322@gmail.com>
2018-01-04 15:56:52 -0800

    Update Pawn.cpp

--- Pawn.cpp
+++ Pawn.cpp
@@ -5,3 +5,33 @@ Pawn::Pawn(int x, int y, bool b){
 	yPos = y;
 	black = b;
 }
+
+void Pawn::move(int xTar, int yTar)
+{
+	while(!checkValid(xTar, yTar))
+	{
+		//get x and y
+	}
+	xPos = xTar;
+	yPos = yTar;
+}
+
+bool Pawn::checkValid(int xTar, int yTar)
+{
+	if(yTar !== yPos + 1)
+	{
+		return False;
+	}
+	else if(xTar == xPos && //piece on square)
+	{
+		return False;
+	}
+	else if((xTar == xPos + 1 || xTar == xPos - 1) && //opposing piece not on square)
+	{
+		return False;
+	}
+	else
+	{
+		return True;
+	}
+}
